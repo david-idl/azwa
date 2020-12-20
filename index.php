feda454a18bbc0b25ed992757e6dc08a12282959
david-idl <david@asyst.fr>
2020-12-20 19:46:24 +0100

    roll back sql server - mis en commentaires

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 	<p>Bienvenue sur ce premier essai</p>
 	<p>Ligne 1</p>
 	<p>Ligne 2</p>
-	<p>MaJ local vers AZure</p>"; 
+	<p>MaJ local vers Azure</p>"; 
 
 
-    $serverName = "idlsqlsrv.database.windows.net"; // update me
-    $connectionOptions = array(
+    echo $serverName = "idlsqlsrv.database.windows.net"; // update me
+ /*   $connectionOptions = array(
         "Database" => "idlsqlbdd000", // update me
         "Uid" => "dbarret", // update me
         "PWD" => "mxdudKg17;" // update me
@@ -22,7 +22,7 @@
     while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
      echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
     }
-    sqlsrv_free_stmt($getResults);
+    sqlsrv_free_stmt($getResults);*/
 	
 	
 ?>
